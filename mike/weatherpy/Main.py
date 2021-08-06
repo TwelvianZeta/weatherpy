6c0442f1523bd0850e25e35ec9b72edaa8de2090
TwelvianZeta <40839581+TwelvianZeta@users.noreply.github.com>
2021-08-06 17:39:01 -0400

    slight changes to output of weatherget function and temp now rounds

--- mike/weatherpy/Main.py
+++ mike/weatherpy/Main.py
@@ -3,13 +3,13 @@ import os
 
 # Functions
 def kToF(inputTemp):
-    return (inputTemp-273.15) * 9/5 + 32
+    return round((inputTemp-273.15) * 9/5 + 32, 2)
 def newLine():
     return "\n"
 def space():
     return " "
 def kToC(inputtemp):
-    return inputtemp-273.15
+    return round(inputtemp-273.15, 2)
 def dot():
     return "."
 
@@ -19,21 +19,22 @@ if(os.getenv("WEATHER_API_KEY") == None):
     exit(2)
 
 # Variables
-version = "1.0+build.5"
+version = "1.0+build.6"
 phase = "SNAPSHOT"
 app_name = "weatherpy"
 degrees = None, 176, 8457, 8451
 
 # Main function
 def weatherget(uszipcode):
-    r = requests.get(
-        "https://api.openweathermap.org/data/2.5/weather?zip="+uszipcode+",us&appid=" + os.getenv("WEATHER_API_KEY"))
+    r = requests.get("https://api.openweathermap.org/data/2.5/weather?zip="+uszipcode+",us&appid=" + os.getenv("WEATHER_API_KEY"))
+    print("Raw data:")
     print(r.json())
+    print()
     print(
         str(r.json()["name"]) + " " +
         "Temperature = " + str(r.json()["main"]["temp"]) + "K " + str(kToF(r.json()["main"]["temp"])) + chr(8457) + " " + str(kToC(r.json()["main"]["temp"])) + chr(8451) + newLine() +
         "ID " + str(r.json()["weather"][0]["id"]) + newLine() +
-        "Weather today: " + str(r.json()["weather"][0]["description"]) + ", weather is " + str(
+        "The weather now is " + str(r.json()["weather"][0]["description"]) + ", or " + str(
             r.json()["weather"][0]["main"]).lower() + dot() + newLine()
     )
 
