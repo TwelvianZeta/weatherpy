fd12db39bbb21a9dc815c7a95bf6c6498a1c99fe
TwelvianZeta <40839581+TwelvianZeta@users.noreply.github.com>
2021-08-06 20:39:57 -0400

    shiftedd stuff, now trycatches

--- mike/weatherpy/Main.py
+++ mike/weatherpy/Main.py
@@ -12,6 +12,24 @@ def kToC(inputtemp):
     return round(inputtemp-273.15, 2)
 def dot():
     return "."
+def printData(r):
+    print("Raw data:")
+    print(r.json())
+    print()
+    print(
+        str(r.json()["name"]) + " " +
+        "Temperature = " + str(r.json()["main"]["temp"]) + "K " + str(kToF(r.json()["main"]["temp"])) + chr(
+            8457) + " " + str(kToC(r.json()["main"]["temp"])) + chr(8451) + newLine() +
+        "Feels like " + str(r.json()["main"]["feels_like"]) + "K " + str(kToF(r.json()["main"]["feels_like"])) + chr(
+            8457) + " " + str(kToC(r.json()["main"]["feels_like"])) + chr(8451) + newLine() +
+        "High " + str(r.json()["main"]["temp_max"]) + "K " + str(kToF(r.json()["main"]["temp_max"])) + chr(
+            8457) + " " + str(kToC(r.json()["main"]["temp_max"])) + chr(8451) + newLine() +
+        "Low " + str(r.json()["main"]["temp_min"]) + "K " + str(kToF(r.json()["main"]["temp_min"])) + chr(
+            8457) + " " + str(kToC(r.json()["main"]["temp_min"])) + chr(8451) + newLine() +
+        "ID " + str(r.json()["weather"][0]["id"]) + newLine() +
+
+        "The weather now is " + str(r.json()["weather"][0]["description"]) + ", or " + str(r.json()["weather"][0]["main"]).lower() + dot() + newLine()
+    )
 
 #Check if api key exists
 if(os.getenv("WEATHER_API_KEY") == None):
@@ -19,7 +37,7 @@ if(os.getenv("WEATHER_API_KEY") == None):
     exit(2)
 
 # Variables
-version = "1.0+build.7"
+version = "1.0-RC.1"
 phase = "SNAPSHOT"
 app_name = "weatherpy"
 degrees = None, 176, 8457, 8451
@@ -27,22 +45,25 @@ degrees = None, 176, 8457, 8451
 # Main function
 def weatherget(uszipcode):
     r = requests.get("https://api.openweathermap.org/data/2.5/weather?zip="+uszipcode+",us&appid=" + os.getenv("WEATHER_API_KEY"))
-    print("Raw data:")
-    print(r.json())
-    print()
-    print(
-        str(r.json()["name"]) + " " +
-        "Temperature = " + str(r.json()["main"]["temp"]) + "K " + str(kToF(r.json()["main"]["temp"])) + chr(8457) + " " + str(kToC(r.json()["main"]["temp"])) + chr(8451) + newLine() +
-        "Feels like " + str(r.json()["main"]["feels_like"]) + "K " + str(kToF(r.json()["main"]["feels_like"])) + chr(8457) + " " + str(kToC(r.json()["main"]["feels_like"])) + chr(8451) + newLine() +
-        "High " + str(r.json()["main"]["temp_max"]) + "K " + str(kToF(r.json()["main"]["temp_max"])) + chr(
-            8457) + " " + str(kToC(r.json()["main"]["temp_max"])) + chr(8451) + newLine() +
-        "Low " + str(r.json()["main"]["temp_min"]) + "K " + str(kToF(r.json()["main"]["temp_min"])) + chr(
-            8457) + " " + str(kToC(r.json()["main"]["temp_min"])) + chr(8451) + newLine() +
-        "ID " + str(r.json()["weather"][0]["id"]) + newLine() +
-        "The weather now is " + str(r.json()["weather"][0]["description"]) + ", or " + str(r.json()["weather"][0]["main"]).lower() + dot() + newLine()
-    )
+    printData(r)
+
+def weatherGetCity(location, country):
+    r = requests.get("https://api.openweathermap.org/data/2.5/weather?q=" + location + "," + country + "&appid=" + os.getenv("WEATHER_API_KEY"))
+    printData(r)
+
+def weatherCity(location):
+    r = requests.get("https://api.openweathermap.org/data/2.5/weather?zip=" + location + "&appid=" + os.getenv("WEATHER_API_KEY"))
+    printData(r)
 
 
 # Start of program
 print(app_name + "\nVersion " + version)
-weatherget(str(input("Input US ZIP code (localization comming soon:tm:)\n")))
+try:
+    weatherget(str(input("Input US ZIP code (localization comming soon:tm:)\n")))
+except:
+    print("failed, location not in us!" + newLine())
+    city = str(input("Input city name" + newLine()))
+    try:
+        weatherGetCity(city, str(input("country (optional)" + newLine())))
+    except:
+        weatherCity(city)
