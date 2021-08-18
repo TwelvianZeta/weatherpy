8a806e048b0afb73fa709afadc147d3043e196a9
TwelvianZeta <40839581+TwelvianZeta@users.noreply.github.com>
2021-08-18 12:32:30 -0400

    apparently 80 is reserved

--- mike/weatherpy/Constants.py
+++ mike/weatherpy/Constants.py
@@ -1,7 +1,7 @@
 # Variables
 version = "1.1-alpha.8" \
           "+" \
-          "build.1"
+          "build.2"
 phase = "ALPHA"
 app_name = "weatherpy"
 degrees = None, 176, 8457, 8451
--- mike/weatherpy/app.py
+++ mike/weatherpy/app.py
@@ -35,4 +35,4 @@ def weather():
     return render_template("weather.html", weather = temperatureC, weatherF = temperatureF, weatherK = temperature)
 
 if __name__ == '__main__':
-    app.run(debug = True, port = 80)
+    app.run(debug = True, port = 8080)
